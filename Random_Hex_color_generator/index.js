725dbc3a032234fb72058407fbc0a0749a604ab0
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-31 20:35:07 +0600

    .\Random_Hex_color_generator\

---
+++ Random_Hex_color_generator/index.js
@@ -0,0 +1,12 @@
+const colorCode = document.querySelector(".color_title")
+const button = document.querySelector(".btn-grad")
+
+
+const generateColorCode = () => {
+    let color = Math.random().toString(16).substring(2, 8)
+    colorCode.innerHTML =`#${color}`
+    colorCode.style.color = `#${color}`
+    document.body.style.backgroundColor = `#${color}`
+}
+
+button.addEventListener("click", generateColorCode)
