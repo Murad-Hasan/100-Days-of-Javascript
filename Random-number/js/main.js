408cf429ad1fe74e8373a39a13d8476d0e4c84ae
Al Mahamud Murad <muradhasan.7845@gmail.com>
2021-12-31 23:59:59 +0600

    .\Random_Hex_color_generator\

--- Random-number/js/main.js
+++ Random-number/js/main.js
@@ -1,9 +1,29 @@
 const btn = document.querySelector('.btn');
 const number = document.querySelector('.random_number')
-
+const reset = document.querySelector('.reset')
+const backgroundColorChange = document.querySelector('.heading')
 
 btn.addEventListener('click', () => {
     const randomNumber = Math.floor(Math.random() * 10) + 1;
     number.innerHTML = `${randomNumber}`;
     number.style.color = '#ea1164';
+})
+
+//background color change
+const backgroundColorCode = () => {
+    const colorCode = Math.random().toString(16).substring(2, 8);
+    document.body.style.backgroundColor = `#${colorCode}`;
+}
+//conent color change
+const contentColorCode = () => {
+    const colorCode = Math.random().toString(16).substring(2, 8);
+    document.querySelector('.content').style.backgroundColor = `#${colorCode}`;
+}
+
+backgroundColorChange.addEventListener('click', backgroundColorCode)
+btn.addEventListener('click', contentColorCode)
+
+reset.addEventListener('click', () => {
+    number.innerHTML = '0';
+    number.style.color = '#0c0c0c';
 })
