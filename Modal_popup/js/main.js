8f86ef6a1a2a2f53b673c5af4472e38c44f1c224
Al Mahamud Murad <muradhasan.7845@gmail.com>
2022-01-02 20:52:30 +0600

    modal_popup

---
+++ Modal_popup/js/main.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector('.modal');
+const btn = document.querySelector('.btn');
+const closeBtn = document.querySelector('.close');
+
+btn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+modal.addEventListener('click', closeModal);
+
+
+//open modal
+function openModal() {
+    modal.style.display = 'block';
+}
+function closeModal() {
+    modal.style.display = 'none';
+}
